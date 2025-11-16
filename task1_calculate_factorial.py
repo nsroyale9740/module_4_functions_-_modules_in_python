5fce8bba08482dcc0c1bcc6966a7fb147a37ba72
nsroyale9740 <nsroyale9740@zohomail.in>
2025-11-16 17:11:01 +0530

    update task1 with for loop, while loop and recursion

--- task1_calculate_factorial.py
+++ task1_calculate_factorial.py
@@ -1,14 +1,42 @@
 n = int(input("Enter a number to calculate its factorial: "))
 
-def factorial(n):
-    if n < 0:
-        return "Factorial is not defined for negative numbers"
-    elif n == 0 or n == 1:
+#with for loop
+
+# def factorial(n):
+#     if n < 0:
+#         return "Factorial is not defined for negative numbers"
+#     elif n == 0 or n == 1:
+#         return 1
+#     else:
+#         factorial = 1
+#         for i in range(1, n + 1):
+#             factorial *= i
+#         return factorial
+
+#with while loop
+
+# def factorial(n):
+#     factorial = 1
+#     while n > 1:
+#         if n < 0:
+#             print(f"Factorial is not defined for negative numbers")
+#             return None
+#         elif n == 0 or n == 1:
+#             print(f"The factorial of {n} is: 1")
+#             return 1
+#         else:
+#             factorial *= n
+#             n -= 1
+#     return factorial
+
+#with recursion
+
+def factorial(num):
+    if num < 0:
+        return "factorial is not defined for negative numbers"
+    elif num == 0 or num == 1:
         return 1
     else:
-        factorial = 1
-        for i in range(1, n + 1):
-            factorial *= i
-        return factorial
+        return num * factorial(num - 1)
 
 print(f"The factorial of {n} is: {factorial(n)}")
